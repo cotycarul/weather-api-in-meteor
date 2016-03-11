07d1941673e140c815e1b908f314c9e5acdf781a
cotycarul <cotycarul@gmail.com>
2016-03-11 14:58:00 +0200

    added new API key

--- server/api.js
+++ server/api.js
@@ -1,7 +1,7 @@
 Meteor.methods({
     checkWeather: function (cityName) {
         this.unblock();
-        var apiKey = "44db6a862fba0b067b1930da0d769e98";
+        var apiKey = "5f4ea7f5349b2b25018b365c8497fe31";
         var api = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
         var results = Meteor.http.call("GET", api);
         if (Meteor.userId()) {
@@ -15,7 +15,6 @@ Meteor.methods({
             });
         }
         else {
-            if (Meteor.userId()) {
                 History.insert({
                     temperature: Math.round(results.data.main.temp - 273.15),
                     weather: results.data.weather[0].main,
@@ -24,14 +23,13 @@ Meteor.methods({
                     owner: undefined,
                     username: "Anonymous"
                 });
-            }
         }
         return results;
     },
 
     checkWeatherByCoord: function (lat, lon) {
         this.unblock();
-        var apiKey = "44db6a862fba0b067b1930da0d769e98";
+        var apiKey = "5f4ea7f5349b2b25018b365c8497fe31";
         var api = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
         var results = Meteor.http.call("GET", api);
         if (Meteor.userId()) {
