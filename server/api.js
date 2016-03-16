5a9fe6adfe034aa91b33abca38b893112448f2e5
cotycarul <cotycarul@gmail.com>
2016-03-16 13:22:12 +0200

    App without the accounts

--- server/api.js
+++ server/api.js
@@ -4,26 +4,12 @@ Meteor.methods({
         var apiKey = "5f4ea7f5349b2b25018b365c8497fe31";
         var api = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
         var results = Meteor.http.call("GET", api);
-        if (Meteor.userId()) {
-            History.insert({
-                temperature: Math.round(results.data.main.temp - 273.15),
-                weather: results.data.weather[0].main,
-                location: cityName,
-                createdAt: new Date(),
-                owner: Meteor.userId(),
-                username: Meteor.user().username
-            });
-        }
-        else {
-                History.insert({
+        History.insert({
                     temperature: Math.round(results.data.main.temp - 273.15),
                     weather: results.data.weather[0].main,
                     location: cityName,
-                    createdAt: new Date(),
-                    owner: undefined,
-                    username: "Anonymous"
+                    createdAt: new Date()
                 });
-        }
         return results;
     },
 
@@ -32,26 +18,12 @@ Meteor.methods({
         var apiKey = "5f4ea7f5349b2b25018b365c8497fe31";
         var api = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
         var results = Meteor.http.call("GET", api);
-        if (Meteor.userId()) {
-            History.insert({
+        History.insert({
                 temperature: Math.round(results.data.main.temp - 273.15),
                 weather: results.data.weather[0].main,
                 location: lat + "; " + lon,
-                createdAt: new Date(),
-                owner: Meteor.userId(),
-                username: Meteor.user().username
+                createdAt: new Date()
             });
-        }
-        else {
-            History.insert({
-                temperature: Math.round(results.data.main.temp - 273.15),
-                weather: results.data.weather[0].main,
-                location: lat + "; " + lon,
-                createdAt: new Date(),
-                owner: undefined,
-                username: "Anonymous"
-            });
-        }
         return results;
     }
 });
